0b601d9cb5d63741617a2dea910f44e74469b3fc
Thomas Van Deusen <thomas.vandeusen@unisys.com>
2019-07-18 15:45:22 -0500

    Markup and positioning improvements for chord cards; add revealer icons

--- js/card.js
+++ js/card.js
@@ -1,13 +1,27 @@
-/*jslint indent: 4 */
+/* jslint indent: 4 */
+
+/*
+ - Plus symbol will start on top
+ - On click, need to transition to minus symbol on top
+      - (by changing the opacity and z-index of both??)
+*/
+
+function setupRevealers() {
+    var revealers = document.getElementsByClassName("revealer");
+    for (let rev of revealers) {
+        rev.click(function() {
+            
+        });
+    }
+}
 
 
 
-
-window.addEventListener("DOMContentLoaded", (event) => {
+window.addEventListener("DOMContentLoaded", event => {
     var cards = document.querySelectorAll(".card");
     for (let card of cards) {
         let cardReveal = card.querySelector(".card-reveal");
-        
+
     }
-    
+
 });
