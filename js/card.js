3125f90f3c8fbdcda678615f7c07a60648456b97
Tom Van Deusen <tvdeusen0892@gmail.com>
2019-07-21 12:27:43 -0500

    Move revealer transition logic to js. Incorrect logic

--- js/card.js
+++ js/card.js
@@ -1,16 +1,23 @@
 /* jslint indent: 4 */
 
-/*
- - Plus symbol will start on top
- - On click, need to transition to minus symbol on top
-      - (by changing the opacity and z-index of both??)
-*/
+function toggleRevealer(revealer) {
+    // This logic isn't working in the way expected.
+    revealer.style.zIndex = (revealer.style.zIndex == "1") ? "2" : "1";
+    revealer.style.opacity = (revealer.style.opacity == "1") ? "0" : "1";
+}
 
 function setupRevealers() {
     var revealers = document.getElementsByClassName("revealer");
     for (let rev of revealers) {
-        rev.click(function() {
-            
+        rev.addEventListener("click", function (event) {
+            let add = this.children.item(0);
+            let remove = this.children.item(1);
+            toggleRevealer(add);
+            toggleRevealer(remove);
+            console.log("adds zIndex is now: %s", add.style.zIndex);
+            console.log("adds opacity is now: %s", add.style.opacity);
+            console.log("removes zIndex is now: %s", remove.style.zIndex);
+            console.log("removes opacity is now: %s", remove.style.opacity);
         });
     }
 }
@@ -18,6 +25,7 @@ function setupRevealers() {
 
 
 window.addEventListener("DOMContentLoaded", event => {
+    setupRevealers();
     var cards = document.querySelectorAll(".card");
     for (let card of cards) {
         let cardReveal = card.querySelector(".card-reveal");
